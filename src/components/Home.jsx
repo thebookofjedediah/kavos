26d4d60ab4e7b6b103de5a3d6b2c8b7c7d7d1c05
Jedediah Arnold <jarnold910@gmail.com>
2021-03-06 09:58:39 -0500

    add packages, setup router, begin scaffolding app

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <>
+            <h1>Home</h1>
+        </>
+    )
+}
+
+export default Home;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.css';
+
+const Nav = () => {
+    return (
+        <>
+            <h1>Nav</h1>
+        </>
+    )
+}
+
+export default Nav;
