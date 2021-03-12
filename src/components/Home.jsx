19839c1229632a2bc76a9f2b62574f385029be1d
Jedediah Arnold <jarnold910@gmail.com>
2021-03-11 19:43:18 -0500

    add coffee and brands to the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 import './Home.css';
+
 
 const Home = () => {
     return (
         <>
             <h1>Home</h1>
+            <ul>
+                <li>
+                    <Link to="/coffees">Coffees</Link>
+                </li>
+                <li>
+                    <Link to="/Brands">Brands</Link>
+                </li>
+            </ul>
         </>
     )
 }
